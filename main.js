4eb55fc9d2a0471e7a8ad669307c4ed45341f8aa
Jimi <jimi.hardee@gmail.com>
2021-10-24 04:44:12 -0700

    added empty form error messages and icons

--- main.js
+++ main.js
@@ -3,22 +3,29 @@ let id = (id) => document.getElementById(id);
 
 let classes = (classes) => document.getElementsByClassName(classes);
 
+let emptyCheck = (index) => {
+  let entryNum = classes("input");
+  if (entryNum[index].value === "") {
+    errorMsg[index].innerHTML = `Please provide your ${entryNum[index].name}`;
+    failureIcon[index].style.opacity = "1";
+  } else {
+    errorMsg[index].innerHTML = "";
+    failureIcon[index].style.opacity = "0";
+    successIcon[index].style.opacity = "1";
+  }
+};
+
 let username = id("username"),
-    email = id("email"),
-    password = id("password"),
-    form = id("form"),
-    errorMsg = document.getElementsByClassName("error"),
-    successIcon = classes("success-icon"),
-    failureIcon = classes("failure-icon");
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault(); // stops flicker error message
-
-    if (username.value === '') {
-        errorMsg[0].innerHTML = 'Please provide a username'
-        failureIcon[0].style.opacity = "1"
-    }
-    else {
-        errorMsg[0].innerHTML = "";
-    }
+  e.preventDefault(); // stops flicker error message
+  for (let i = 0; i < 3; i++) {
+    emptyCheck(i);
+  }
 });
